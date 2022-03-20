5e9a097ae5b459b1ad54120c4df12f70a36e673d
Gustavo <gusta.p.o@hotmail.com>
2022-03-20 14:26:25 +0000

    feat: Config ProducerDemo and send messages

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-exploring-kafka
+#Exploring Apache Kafka with Java
+
+###CLI:
+- Create kafka topic: 
+>kafka-topics --bootstrap-server 127.0.0.1:9092 --create --topic demo_java --partitions 3 --replication-factor 1
+
+- Start kafka console consumer:
+>kafka-console-consumer --bootstrap-server 127.0.0.1:9092 --topic demo_java
---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,40 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String [] args){
+        log.info("I'm a Kafka Producer!");
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java","Hello World");
+
+        // Send the data - Asynchronous
+        producer.send(producerRecord);
+
+        // Flush Data - Synchronous
+        producer.flush();
+
+        // Flush and close producer
+        producer.close();
+
+    }
+}
