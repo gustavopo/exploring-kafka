160bc9f6747295ae908f897294207d62690abcb4
Gustavo <gusta.p.o@hotmail.com>
2022-03-20 14:26:57 +0000

    feat: Config ProducerDemoWithCallback

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,59 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+
+    public static void main(String [] args){
+        log.info("I'm a Kafka Producer!");
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java","Hello World");
+
+        // Send the data - Asynchronous
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception e) {
+                // executes everytime a record is sucessfully sent or an exception is thrown
+                if( e == null){
+                    //record sucessfully sent
+                    log.info("Received new metadata / \n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp() + "\n"
+
+                    );
+                }else{
+                    log.error("Error while producing: ", e.getMessage());
+                }
+            }
+        });
+
+        // Flush Data - Synchronous
+        producer.flush();
+
+        // Flush and close producer
+        producer.close();
+
+    }
+}
